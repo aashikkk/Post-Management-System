11866c9f01eb3488a4d0be7a5fba5df93777fc63
Aashik Shihaab <sihaabama@gmail.com>
2024-06-09 03:54:30 +0530

    localStorage Setup

--- src/slices/postsSlice.js
+++ src/slices/postsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    postsList: [],
+    postsList: JSON.parse(localStorage.getItem("posts")) || [],
     selectedPost: {},
 };
 
@@ -13,16 +13,19 @@ const postsSlice = createSlice({
             const id = Math.random() * 100;
             let post = { ...action.payload, id };
             state.postsList.push(post);
+            localStorage.setItem("posts", JSON.stringify(state.postsList));
         },
         removePostFromList: (state, action) => {
             state.postsList = state.postsList.filter(
                 (post) => post.id !== action.payload.id
             );
+            localStorage.setItem("posts", JSON.stringify(state.postsList));
         },
         updatePostInList: (state, action) => {
             state.postsList = state.postsList.map((post) =>
                 post.id === action.payload.id ? action.payload : post
             );
+            localStorage.setItem("posts", JSON.stringify(state.postsList));
         },
         setSelectedPost: (state, action) => {
             state.selectedPost = action.payload;
