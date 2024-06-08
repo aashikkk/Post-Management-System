912552fafdf5815e2be5f737e34c64258b0d9f89
Aashik Shihaab <82132513+aashikkk@users.noreply.github.com>
2024-06-09 04:31:39 +0530

    rename

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 import { addPostToList } from "../slices/postsSlice";
 import { useDispatch } from "react-redux";
 
-export const AddTask = () => {
+export const AddPost = () => {
     const dispatch = useDispatch();
 
     const [values, setValues] = useState({
